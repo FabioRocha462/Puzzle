d046b00f31a63b71eb2e0bd0dac6190406d2c9b8
Fábio Rocha <rochafabio462@gmail.com>
2023-07-11 16:04:07 -0300

    ainda não funciona

--- regras.pl
+++ regras.pl
@@ -1,36 +1,37 @@
-:- dynamic initial_state/1.
-:- dynamic goal_state/1.
+:- use_module(library(lists)).
 
-% Regras para movimentar as peças
-move_left([X, '', Y, Z | Tail], [ '', X, Y, Z | Tail]).
-move_left([X1, X2, X3, X4 | Tail], [X1, X2, X3, X4 | NewTail]) :-move_left([X2, X1, X3, X4 | Tail], NewTail).
+% Operações para movimentar as peças
+move([X, '', Y, Z | Tail], [ '', X, Y, Z | Tail]).
+move([X1, X2, X3, X4 | Tail], [X1, X2, X3, X4 | NewTail]) :-move([X2, X1, X3, X4 | Tail], NewTail).
 
-move_right([ '', X, Y, Z | Tail], [X, '', Y, Z | Tail]).
-move_right([X1, X2, X3, X4 | Tail], [X1, X2, X3, X4 | NewTail]) :-move_right([X2, X1, X3, X4 | Tail], NewTail).
+move([X, Y, '', Z | Tail], [X, Y, Z, '' | Tail]).
+move([X1, X2, X3, X4 | Tail], [X1, X2, X3, X4 | NewTail]) :-move([X1, X2, X3, X4 | Tail], NewTail).
 
-move_up([X1, X2, '', X3 | Tail], [X1, '', X2, X3 | Tail]).
-move_up([X1, X2, X3, X4 | Tail], [X1, X2, X3, X4 | NewTail]) :-move_up([X1, X2, X3, X4 | Tail], NewTail).
+move([X1, '', X2, X3 | Tail], [X1, X2, '', X3 | Tail]).
+move([X1, X2, X3, X4 | Tail], [X1, X2, X3, X4 | NewTail]) :- move([X1, X2, X3, X4 | Tail], NewTail).
 
-move_down([X1, X2, X3, '', X4 | Tail], [X1, X2, X3, X4 | Tail]).
-move_down([X1, X2, X3, X4 | Tail], [X1, X2, X3, X4 | NewTail]) :-move_down([X1, X2, X3, X4 | Tail], NewTail).
+move(['', X, Y, Z | Tail], [X, '', Y, Z | Tail]).
+move([X1, X2, X3, X4 | Tail], [X1, X2, X3, X4 | NewTail]) :- move([X1, X2, X3, X4 | Tail], NewTail).
 
 % Regra para verificar se o estado atual é o objetivo
 goal_state(State) :- State = [1, 2, 3, 4, 5, 6, 7, 8, ''].
 
-% Regra para resolver o Slider Puzzle
-solve(Solution) :-initial_state(InitialState),solve_puzzle([InitialState], [], Solution).
+% Regra para gerar movimentos possíveis
+generate_moves(State, Moves) :-findall(NewState, move(State, NewState), Moves).
 
-% Regras auxiliares para gerar movimentos possíveis
-generate_moves(State, Moves) :- findall(NewState, move(State, NewState), Moves).
-
-% Regra auxiliar para aplicar um movimento a um estado
+% Regra para aplicar um movimento a um estado
 apply_move(State, Move, NewState) :- move(State, NewState), goal_state(Move).
 
-% Regra auxiliar para buscar a solução utilizando busca em largura
-breadth_first_search([[State|Path]|_], _, [State|Path]) :- goal_state(State).
-breadth_first_search([[State|Path]|RestQueue],Visited,Solution) :- goal_state(State),\+ member(State,Visited),generate_moves(State,Moves),append(RestQueue, NewQueue, [[State|Path]|RestQueue]),add_to_visited(Moves, State, Visited, NewVisited),breadth_first_search(NewQueue, NewVisited, Solution).
+% Regra para buscar a solução utilizando busca em largura
+breadth_first_search([[State|Path]|_], _, [State|Path]) :-goal_state(State).
+breadth_first_search([[State|Path]|RestQueue], Visited, Solution):-  goal_state(State), member(State, Visited),generate_moves(State, Moves),append(RestQueue, NewQueue, [[State|Path]|RestQueue]),add_to_visited(Moves, State, Visited, NewVisited),breadth_first_search(NewQueue, NewVisited, Solution).
 
 % Regra auxiliar para adicionar novos estados à lista de visitados
 add_to_visited([], _, Visited, Visited).
-add_to_visited([Move|Moves],State,Visited,NewVisited):- member(Move,Visited),add_to_visited(Moves, State, [Move|Visited], NewVisited).
-add_to_visited([_|Moves], State, Visited, NewVisited):-add_to_visited(Moves, State, Visited, NewVisited).
+add_to_visited([Move|Moves], State, Visited, NewVisited) :- member(Move, Visited),add_to_visited(Moves, State, [Move|Visited], NewVisited).
+add_to_visited([_|Moves], State, Visited, NewVisited) :-add_to_visited(Moves, State, Visited, NewVisited).
+
+% Regra para resolver o Eight Puzzle
+solve(Solution) :-initial_state(InitialState),breadth_first_search([[InitialState]], [], Solution).
+
+
--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 from pyswip import Prolog
+import random
 
-def solve_slider_puzzle(initial_state, goal_state):
-    prolog = Prolog()
-    prolog.consult("regras.pl")  # Carrega as regras Prolog
-
-    # Converte o estado inicial e o estado objetivo em uma lista de Prolog
+prolog = Prolog()
+prolog.consult("regras.pl") 
+def solve_eight_puzzle(initial_state):
+    # Converte o estado inicial em uma lista de Prolog
     initial_state_list = list(initial_state)
-    goal_state_list = list(goal_state)
     prolog.assertz("initial_state({})".format(initial_state_list))
-    prolog.assertz("goal_state({})".format(goal_state_list))
 
     # Encontra a solução usando Prolog
     solutions = list(prolog.query("solve(Solution)"))
@@ -18,11 +16,10 @@ def solve_slider_puzzle(initial_state, goal_state):
         return solutions[0]["Solution"]
     else:
         return None
-
-# Exemplo de uso
-initial_state = [2, 1, 3, 4, 5, 6, 7, 8, '']
-goal_state = [1, 2, 3, 4, 5, 6, 7, 8, '']
-solution = solve_slider_puzzle(initial_state, goal_state)
+    
+initial_state = [1, 2, 3, 4, 5, 6, 7, 8, '']
+random.shuffle(initial_state)
+solution = solve_eight_puzzle(initial_state)
 
 if solution:
     print("Solução encontrada:")
@@ -30,3 +27,4 @@ if solution:
         print(step)
 else:
     print("Não foi possível encontrar uma solução.")
+
